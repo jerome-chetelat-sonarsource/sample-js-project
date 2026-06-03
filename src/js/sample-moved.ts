13fe711c27508afec7b8838686b46c5f2e86f4f8
Jerome Chetelat <jerome.chetelat@sonarsource.com>
2026-06-03 11:48:51 +0200

    fix typings

--- src/js/sample-moved.ts
+++ src/js/sample-moved.ts
@@ -1,12 +1,12 @@
 let n: number = 1;
 alert('n is ' + n);
 
-function foo(a, b, c) {
+function foo(a: any, b: any, c: any) {
   var d;
   if (a) {
     if (b) {
-      while (c ? d=12 : d=15) {
-        if (d==12 || c==14 && a==b) {
+      while (c ? d = 12 : d = 15) {
+        if (d == 12 || c == 14 && a == b) {
           return a;
         }
       }
